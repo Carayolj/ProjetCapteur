e7ecc44ea1f9b81714b85fe19d69a51246af90ab
Carayolj <julien.carayol@gmail.com>
2017-09-14 14:41:12 +0200

    [julien] bouton+capteur

--- src/Integr_.ino
+++ src/Integr_.ino
@@ -1,16 +1,36 @@
 #include "DHT.h"
 
-int bouton = 4;
-int led = 13 ;
+DHT dht;
+float humidity;
+float temperature;
+int pinSensor=2;
+int pinBouton=4;
+int btn;
+void setup()
+{
+  pinMode(pinBouton, INPUT);
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
 
-void setup() {
-  pinMode(bouton, INPUT);
-  pinMode(led, OUTPUT);
-
+  dht.setup(pinSensor);
 }
-void loop() {
-  int btn=digitalRead(bouton);
-  digitalWrite(led, btn);
-  delay(1000);
+
+void loop()
+{
+        delay(50);
+btn=digitalRead(pinBouton);
+        if(btn==HIGH)    {
+         humidity = dht.getHumidity();
+         temperature = dht.getTemperature();
+
+        Serial.print(dht.getStatusString());
+        Serial.print("\t");
+        Serial.print(humidity, 1);
+        Serial.print("\t\t");
+        Serial.print(temperature, 1);
+        Serial.print("\t\t");
+        Serial.println(dht.toFahrenheit(temperature), 1);
+      }
 
 }
